8e9e75cd94c97ca91da84b75eba61ad79f733cd7
CoreDoor <32739464+rishabh04gandhi@users.noreply.github.com>
2021-05-21 11:42:50 +0530

    Initial commit

---
+++ app/src/main/java/com/rishabhg4/mymemory/models/UserImageList.kt
@@ -0,0 +1,7 @@
+package com.rishabhg4.mymemory.models
+
+import com.google.firebase.firestore.PropertyName
+
+data class UserImageList (
+    @PropertyName("images") val images: List<String>? = null
+)
